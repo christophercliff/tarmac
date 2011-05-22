66e3d224753531b3fd4eef32f1326db97409c9c0
Christopher Cliff <ctcliff@gmail.com>
2011-05-22 13:47:55 -0400

    created view for retrieving types--seems wonky, research this;

---
+++ views/types/map.js
@@ -0,0 +1,10 @@
+
+function(doc){
+    if (doc.type === 'Feature')
+    {
+        if (doc.properties && doc.properties.type)
+        {
+            emit(doc.properties.type, null);
+        }
+    }
+};
